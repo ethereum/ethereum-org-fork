18fdb233e71d16af900eb1d663fad6e0ee45a95c
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-11-21 13:43:39 -0500

    update BannerGrid component

--- src/components/BannerGrid/index.tsx
+++ src/components/BannerGrid/index.tsx
@@ -1,74 +1,58 @@
-import React from "react"
 import { Box, Flex, Grid, useToken } from "@chakra-ui/react"
+import type { ChildOnlyProp } from "@/lib/types"
 
-export type Props = {
-  children: React.ReactNode
-}
+export const Banner = ({ children }: ChildOnlyProp) => (
+  <Flex
+    w="full"
+    background="bannerGridGradient"
+    direction={{ base: "column", lg: "row" }}
+    wrap="nowrap"
+    sx={{
+      h2: { mt: 0 },
+      ul: { mb: 0 },
+    }}
+  >
+    {children}
+  </Flex>
+)
 
-export const Banner: React.FC<Props> = ({ children }) => {
-  return (
-    <Flex
-      w="full"
-      background="bannerGridGradient"
-      direction={{ base: "column", lg: "row" }}
-      wrap="nowrap"
-      sx={{
-        h2: {
-          mt: 0,
-        },
-        ul: {
-          mb: 0,
-        },
-      }}
-    >
-      {children}
-    </Flex>
-  )
-}
+export const BannerBody = ({ children }: ChildOnlyProp) => (
+  <Box flex={4} p="10">
+    {children}
+  </Box>
+)
 
-export const BannerBody: React.FC<Props> = ({ children }) => {
-  return (
-    <Box flex={4} p={10}>
-      {children}
-    </Box>
-  )
-}
+export const BannerImage = ({ children }: ChildOnlyProp) => (
+  <Flex justifyContent="end" flex={2} alignSelf="end">
+    {children}
+  </Flex>
+)
 
-export const BannerImage: React.FC<Props> = ({ children }) => {
-  return (
-    <Flex justifyContent="end" flex={2} alignSelf="end">
-      {children}
-    </Flex>
-  )
-}
+export const BannerGrid = ({ children }: ChildOnlyProp) => (
+  <Grid
+    templateColumns={{
+      base: "repeat(1,1fr)",
+      md: "repeat(2,1fr)",
+      lg: "repeat(3,1fr)",
+    }}
+    templateRows={{
+      md: "repeat(3, 1fr)",
+      lg: "repeat(2, 1fr)",
+    }}
+    columnGap="0"
+    rowGap="0"
+  >
+    {children}
+  </Grid>
+)
 
-export const BannerGrid: React.FC<Props> = ({ children }) => {
-  return (
-    <Grid
-      templateColumns={{
-        base: "repeat(1,1fr)",
-        md: "repeat(2,1fr)",
-        lg: "repeat(3,1fr)",
-      }}
-      templateRows={{
-        md: "repeat(3, 1fr)",
-        lg: "repeat(2, 1fr)",
-      }}
-      columnGap={0}
-      rowGap={0}
-    >
-      {children}
-    </Grid>
-  )
-}
-
-export const BannerGridCell: React.FC<Props> = ({ children }) => {
+export const BannerGridCell = ({ children }: ChildOnlyProp) => {
   const [medBp, lgBp] = useToken("breakpoints", ["md", "lg"])
 
   return (
     <Flex
-      px={{ base: 0, md: 12 }}
-      py={8}
+      px={{ base: 0, md: "12" }}
+      py="8"
       direction="column"
       borderTop="1px solid"
       borderTopColor="searchBackground"
