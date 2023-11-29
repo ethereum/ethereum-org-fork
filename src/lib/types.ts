53bed4112045e159c721c3f5f8e90c21db80947b
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-11-28 16:54:03 -0800

    update associated get-eth page components

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -160,8 +160,8 @@ export type ToCNodeEntry = {
 export type TocNodeType =
   | ToCNodeEntry
   | {
-      items: TocNodeType[]
-    }
+    items: TocNodeType[]
+  }
 
 export type ToCItem = {
   title: string
